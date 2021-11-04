06d54ced8567ae64a5950389ffdcadb8ff6e6120
Harrison <h.alexander.barnett@gmail.com>
2021-11-04 18:22:22 -0400

    Adding some assets

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
+import background from '../assets/images/header-bg.jpeg'
+import title from '../assets/images/header-title.png'
 const Header = () => {
     return(
         <div 
-        className='header'></div>
+        className='header'>
+            <img
+            src={background}></img>
+            <img
+            src={title}></img>
+        </div>
     )
 }
 export default Header
